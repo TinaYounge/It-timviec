708d726cf726e74a7719932d8502888b07bc73fa
Tina <nguyennga.hcm@gmail.com>
2021-07-14 21:21:13 +0700

    4

--- api/companies.api.js
+++ api/companies.api.js
@@ -5,7 +5,7 @@ const { jobs } = require("../data/data.json");
 
 //create
 
-const addablecompaniesParams = ["companiesName", "stock"];
+// const addablecompaniesParams = ["companiesName", "stock"];
 router.post("/", (req, res) => {
   try {
     const company = {};
@@ -20,7 +20,9 @@ router.post("/", (req, res) => {
   }
 });
 
-const filterablecompanyParams = ["city", "name"];
+// const filterablecompanyParams = ["city", "name"];
+// http:localhost:5000/companies?page=5
+// http:localhost:5000/companies?/city
 
 router.get("/", (req, res) => {
   try {
@@ -31,24 +33,26 @@ router.get("/", (req, res) => {
       const b = nth * 20 - 1;
       res.status(202).send(companies.slice(a, b));
     } else if (city) {
-      // city;
-      const cityCompanies = req.query.city;
-      console.log("cityCompanies", cityCompanies);
-      const cityJobList = jobs.filter((params) => params.city == cityCompanies);
-      const companiesIdList = [];
-      for (let i of cityJobList) {
-        companiesIdList.push(i.companyId);
+      const city = req.query.city;
+      const cities = city.split(",");
+      jobsLocatedList = [];
+      for (let i of jobs) {
+        for (let j of cities) {
+          if (i.city == j) {
+            jobsLocatedList.push(i.companyId);
+          }
+        }
       }
       companiesLocatedList = [];
       for (let i of companies) {
-        for (let j of companiesIdList) {
+        for (let j of jobsLocatedList) {
           if (i.id == j) {
             companiesLocatedList.push(i.name);
           }
         }
       }
       const result = [...new Set(companiesLocatedList)];
-      res.status(200).send(result);
+      res.status(202).send(result);
     } else {
       res.status(202).send(companies.slice(0, 20));
     }
